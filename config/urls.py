aa6f213eb70065ca8f604b93604a2475dfbfe228
Mohammad Erfan Pouladzadeh <86192924+MErfanPld@users.noreply.github.com>
2022-10-14 19:51:27 +0330

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,17 +5,17 @@ from config.api.urls import urlpatterns
 from .views import (ContactDetailView,
                     ContactListView, PanelView, LandingView)
 
-# from .views import (AboutUsCreateView, AboutUsView, ContactDetailView,
-#                     ContactListView, PanelView, RuleCreateView, RuleView, LandingView)
+from .views import (AboutUsCreateView, AboutUsView, ContactDetailView,
+                PanelView, RuleCreateView, RuleView, LandingView)
 
 
 app_name = "config"
 
 urlpatterns = [
-    # path("about_us/", AboutUsView.as_view(), name="about_us"),
-    # path("about_create/", AboutUsCreateView.as_view(), name="about_create"),
-    # path("rule_create/", RuleCreateView.as_view(), name="rule_create"),
-    # path("rule/", RuleView.as_view(), name="rule"),
+    path("about_us/", AboutUsView.as_view(), name="about_us"),
+    path("about_create/", AboutUsCreateView.as_view(), name="about_create"),
+    path("rule_create/", RuleCreateView.as_view(), name="rule_create"),
+    path("rule/", RuleView.as_view(), name="rule"),
     path("contact_list/", ContactListView.as_view(), name="contact_list"),
     path(
         "contact_detail/<int:pk>/", ContactDetailView.as_view(), name="contact_detail"
